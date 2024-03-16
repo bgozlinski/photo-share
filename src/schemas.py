bfb2bf496c1cff941f59e3b75ecae8ac9d28b223
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-03-16 12:37:00 +0100

    Modified UsersModel Schema

--- src/schemas.py
+++ src/schemas.py
@@ -1,11 +1,8 @@
-from datetime import datetime
-
 from pydantic import BaseModel
 
 
 class UsersModel(BaseModel):
     username: str
     email: str
-    is_active: bool
-    role: str
-    created_at: datetime
+    password: str
+
