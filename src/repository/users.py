64cb52080a6c308a321c7274fafcbb416aac91cf
bgozlinski <bartlomiej.gozlinski@gmail.com>
2024-03-26 23:41:43 +0100

    Added update_user logic and endpoint. Username and Email can be updated by endpoint. Rise error if new Username or email already exists.

--- src/repository/users.py
+++ src/repository/users.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from src.auth import get_password_hash
 from src.database.models import Users
-from src.schemas import UsersModel, UsersResponseModel
+from src.schemas import UsersModel, UsersUpdateModel
 
 
 async def create_user(user_data: UsersModel,
@@ -19,7 +19,7 @@ async def create_user(user_data: UsersModel,
     ).first()
 
     if existing_user:
-        raise HTTPException(status_code=409, detail="Username or email already exists")
+        raise HTTPException(status_code=400, detail="Username or email already exists")
 
     hashed_password = get_password_hash(user_data.password)
     user_dict = user_data.dict()
@@ -49,3 +49,43 @@ async def delete_user(user_id: int,
     user = db.query(Users).filter(Users.id == user_id).first()
     db.delete(user)
     db.commit()
+
+
+async def update_user(user_id: int,
+                      update_data: UsersUpdateModel,
+                      db: Session
+                      ) -> Users:
+
+    user = db.query(Users).filter(Users.id == user_id).first()
+
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+
+    if update_data.username:
+        existing_user = db.query(Users).filter(
+            Users.username == update_data.username,
+            Users.id != user_id
+        ).first()
+
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Username already exists.")
+
+    if update_data.email:
+        existing_user = db.query(Users).filter(
+            Users.email == update_data.email,
+            Users.id != user_id
+        ).first()
+
+        if existing_user:
+            raise HTTPException(status_code=400, detail="Email already exists.")
+
+    if update_data.username:
+        user.username = update_data.username
+    if update_data.email:
+        user.email = update_data.email
+
+    db.commit()
+    db.refresh(user)
+
+    return user
+
